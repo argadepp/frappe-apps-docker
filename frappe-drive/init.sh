73523bdcf2df9b564d87f97115e49334e074df48
pratik3686 <pratik3686@gmail.com>
2024-02-21 17:37:51 +0530

    frappe drive added

---
+++ frappe-drive/init.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+    echo "Bench already exists, skipping init"
+    cd frappe-bench
+    bench start
+else
+    echo "Creating new bench..."
+fi
+
+bench init --skip-redis-config-generation frappe-bench
+
+cd frappe-bench
+
+# Use containers instead of localhost
+bench set-mariadb-host mariadb
+bench set-redis-cache-host redis:6379
+bench set-redis-queue-host redis:6379
+bench set-redis-socketio-host redis:6379
+
+# Remove redis, watch from Procfile
+sed -i '/redis/d' ./Procfile
+sed -i '/watch/d' ./Procfile
+
+bench get-app https://github.com/frappe/drive
+
+bench new-site drive
+
+bench --site drive add-to-hosts
+
+bench --site drive install-app drive
+
+bench start
+
+cd apps/drive && yarn dev
