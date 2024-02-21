3362aa04c12044da94db4f10d01ec7856dec577f
pratik3686 <pratik3686@gmail.com>
2024-02-21 16:19:40 +0530

    docker-compose frappe apps support

---
+++ frappe-helpdesk/init.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+    echo "Bench already exists, skipping init"
+    cd frappe-bench
+    bench start
+else
+    echo "Creating new bench..."
+fi
+
+bench init --skip-redis-config-generation frappe-bench
+
+cd frappe-bench
+
+# Use containers instead of localhost
+bench set-mariadb-host mariadb
+bench set-redis-cache-host redis:6379
+bench set-redis-queue-host redis:6379
+bench set-redis-socketio-host redis:6379
+
+# Remove redis, watch from Procfile
+sed -i '/redis/d' ./Procfile
+sed -i '/watch/d' ./Procfile
+
+bench get-app helpdesk
+
+bench new-site helpdesk --force --no-mariadb-socket --admin-password=admin --db-host=mariadb --db-root-password=123 --install-app helpdesk --set-default
+
+
+bench start
